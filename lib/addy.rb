7a99e8970f4a04342eedee599f8002a8403743ac
Allen Madsen <blatyo@gmail.com>
2010-06-28 19:24:36 -0400

    Added ability for addy to call sum on self.

--- lib/addy.rb
+++ lib/addy.rb
@@ -1,6 +1,8 @@
 module Addy
-  def summation(range)
-    range.inject(0) do |memo, num|
+  def summation(injectable = nil)
+    injectable ||= self
+    
+    injectable.inject(0) do |memo, num|
       if block_given?
         yield(num) + memo
       else
