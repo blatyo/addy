faebd100b9fbf0fc008326dbff064ad75e737137
Allen Madsen <blatyo@gmail.com>
2010-06-28 19:51:09 -0400

    Added raise when inject is not implemented on the object sum is called on.

--- lib/addy.rb
+++ lib/addy.rb
@@ -2,6 +2,8 @@ module Addy
   def summation(injectable = nil)
     injectable ||= self
     
+    raise "#{injectable.class.name} does not implement inject." unless injectable.respond_to? :inject
+    
     injectable.inject(0) do |memo, num|
       if block_given?
         yield(num) + memo
